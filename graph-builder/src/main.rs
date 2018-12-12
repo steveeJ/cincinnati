b381325130e0358fc8afe92116eda82ce4cef277
Stefan Junker <mail@stefanjunker.de>
2018-12-12 14:27:01 +0100

    graph-builder: add ScopeTracker allocator
    This custom allocator helps tracking memory allocations which aren't
    free'd up at the end of their scope.
    
    There's one gotcha:
    The values which are reported to escape the scope also include return
    arguments.

--- graph-builder/src/main.rs
+++ graph-builder/src/main.rs
@@ -77,3 +77,95 @@ fn main() -> Result<(), Error> {
     .run();
     Ok(())
 }
+use self::scopetracker::AllocationTracker;
+
+#[global_allocator]
+static GLOBAL: AllocationTracker = AllocationTracker::new();
+
+mod scopetracker {
+    use super::GLOBAL;
+
+    use std::alloc::{GlobalAlloc, Layout, System};
+    use std::sync::atomic::{AtomicIsize, Ordering};
+
+    pub struct AllocationTracker {
+        mem: AtomicIsize,
+    }
+
+    impl AllocationTracker {
+        pub const fn new() -> Self {
+            AllocationTracker {
+                mem: AtomicIsize::new(0),
+            }
+        }
+
+        fn current_mem(&self) -> isize {
+            self.mem.load(Ordering::SeqCst)
+        }
+    }
+
+    unsafe impl GlobalAlloc for AllocationTracker {
+        unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+            self.mem.fetch_add(layout.size() as isize, Ordering::SeqCst);
+            System.alloc(layout)
+        }
+
+        unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+            self.mem.fetch_sub(layout.size() as isize, Ordering::SeqCst);
+            System.dealloc(ptr, layout)
+        }
+    }
+
+    pub struct ScopeTracker<'a> {
+        at_start: isize,
+        name: &'a str,
+        file: &'static str,
+        line: u32,
+    }
+
+    impl<'a> ScopeTracker<'a> {
+        pub fn new(name: &'a str, file: &'static str, line: u32) -> Self {
+            Self {
+                at_start: GLOBAL.current_mem(),
+                name,
+                file,
+                line,
+            }
+        }
+    }
+
+    impl Drop for ScopeTracker<'_> {
+        fn drop(&mut self) {
+            let old = self.at_start;
+            let new = GLOBAL.current_mem();
+            if old != new {
+                if self.name == "" {
+                    debug!(
+                        "{}:{}: {} bytes escaped scope",
+                        self.file,
+                        self.line,
+                        new - old
+                    );
+                } else {
+                    debug!(
+                        "{}:{} '{}': {} bytes escaped scope",
+                        self.file,
+                        self.line,
+                        self.name,
+                        new - old
+                    );
+                }
+            }
+        }
+    }
+
+    #[macro_export]
+    macro_rules! mem_guard {
+        () => {
+            mem_guard!("")
+        };
+        ($e:expr) => {
+            let _guard = ScopeTracker::new($e, file!(), line!());
+        };
+    }
+}
